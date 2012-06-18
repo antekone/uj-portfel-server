e3d403e8f2489fc16f45af8251099aa397df0ff9
Szymon Czarnecki <spirit_sc@thinkdifferent.pl>
2012-06-18 11:40:12 +0200

    Poprawki do zaproszen

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -12,5 +12,16 @@ class Invitation < ActiveRecord::Base
   
   validates :user_id, presence: true
   validates :account_id, presence: true
+  validates :recipient_email, presence: true
   validates :state, :inclusion => { :in => STATES.values }
+  
+  before_create :find_recipient
+  
+  def state_name
+    STATES.detect { |k, v| v == self.state }.first
+  end
+  
+  def find_recipient
+    self.recipient ||= User.find_by_email(self.recipient_email.downcase)
+  end
 end
