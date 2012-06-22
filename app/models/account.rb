0c8d3566f647cf42a37b2b5413f43c201a35460e
Szymon Czarnecki <spirit_sc@thinkdifferent.pl>
2012-06-22 20:34:13 +0200

    Domsylna nazwa dla nowo tworzonego konta

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 class Account < ActiveRecord::Base
   belongs_to :user
   has_many :transactions, dependent: :delete_all
@@ -7,12 +8,18 @@ class Account < ActiveRecord::Base
   validates :user_id, presence: true
   validates :currency, presence: true
   
+  before_create :complete_name
+  
   def balance
     read_attribute(:balance_in_cents).to_f/100
   end
   
   def balance=(balance)
     write_attribute(:balance_in_cents, balance.gsub(",", ".").to_f*100)
+  end
+  
+  def complete_name
+    write_attribute(:name, "Domyślne") unless self.name?
   end
   
   def as_json(options={})
